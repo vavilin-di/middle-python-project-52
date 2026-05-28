382aad96df978ea46d9d0d1150635b7859fff739
div <div@nowhe.re>
2026-05-28 22:47:52 +0700

    Актуализирован список зависимостей и зарегистрированных приложений Django.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_bootstrap5",
+    "django_filters",
     "task_manager.applications.users",
     "task_manager.applications.statuses",
     "task_manager.applications.tasks",
