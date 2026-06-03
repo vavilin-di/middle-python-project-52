aca0036e5c48170cd55a9de684f3f7e3cb44de6f
div <div@nowhe.re>
2026-06-03 16:48:44 +0700

    Добавлена ленивая инициализация класса.

--- task_manager/utilities/aggregates.py
+++ task_manager/utilities/aggregates.py
@@ -1,24 +1,42 @@
-__all__ = ["ArrayAggregation"]
+__all__ = ["ArrayAggregation", "get_array_aggregation"]
+
+from typing import Self
 
 from django.contrib.postgres.aggregates import ArrayAgg
 from django.db import connection
 from django.db.models import Aggregate, JSONField, Value
 
-match connection.vendor:
-    case "postgresql":
-        ArrayAggregation = ArrayAgg
-    case "sqlite":
 
-        class ArrayAggregation(Aggregate):  # type: ignore[no-redef]
-            function = "JSON_GROUP_ARRAY"
-            output_field = JSONField()
-            template = "%(function)s(%(distinct)s%(expressions)s)"
+class SQLiteArrayAggregation(Aggregate):
+    """Аналог агрегатной функции ArrayAgg для SQLite"""
 
-            def resolve_expression(self, query, allow_joins=True, reuse=None, summarize=False, for_save=False):
-                if self.default is not None and isinstance(self.default, Value):
-                    self.default = Value("[]", output_field=JSONField())
-                return super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
+    function = "JSON_GROUP_ARRAY"
+    output_field = JSONField()
+    template = "%(function)s(%(distinct)s%(expressions)s)"
 
-    case _:
-        msg = f"Unsupported database vendor {connection.vendor}"
-        raise AssertionError(msg)
+    def resolve_expression(self, query, allow_joins=True, reuse=None, summarize=False, for_save=False) -> Self:  # type: ignore[override]
+        if self.default is not None and isinstance(self.default, Value):  # type: ignore[has-type]``
+            self.default = Value("[]", output_field=JSONField())
+        return super().resolve_expression(query, allow_joins, reuse, summarize, for_save)
+
+
+def get_array_aggregation() -> type[ArrayAgg] | type[SQLiteArrayAggregation]:
+    """Возвращает класс агрегатной функции в зависимости от БД.
+
+    Returns:
+        ArrayAgg для PostgreSQL, SQLiteArrayAggregation для SQLite.
+
+    Raises:
+        AssertionError: если БД не поддерживается.
+    """
+    match connection.vendor:
+        case "postgresql":
+            return ArrayAgg
+        case "sqlite":
+            return SQLiteArrayAggregation
+        case _:
+            msg = f"Неподдерживаемый вендор базы данных: {connection.vendor}"
+            raise AssertionError(msg)
+
+
+ArrayAggregation: type[ArrayAgg] | type[SQLiteArrayAggregation] = get_array_aggregation()
